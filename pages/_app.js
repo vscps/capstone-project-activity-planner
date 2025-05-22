c6a79a85b1614640abc275219afa49e0da087ffd
Polarchris <chris@polarchris.com>
2025-05-22 16:11:14 +0200

    added: Global Navigation Component , just functional, not styled yet

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
 import GlobalStyle from "../styles";
+import GlobalNavigation from "../components/Navigation/Navigation";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <GlobalNavigation />
     </>
   );
 }
