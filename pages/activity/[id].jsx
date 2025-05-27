d0bcd697cbf63a7d61d6f93789a31f20929ffa55
Gregor Wedlich <hello@sil3ntrunning.net>
2025-05-27 11:55:01 +0200

    Add: API route for single activity Refactor: Some little cleanups

--- pages/activity/[id].jsx
+++ pages/activity/[id].jsx
@@ -1,22 +1,19 @@
 import { useRouter } from "next/router";
 import useSWR from "swr";
-import { Fragment } from "react";
 import ActivityDetail from "@/components/ActivityDetail/ActivityDetail";
 
 export default function Page() {
-  const { data, error } = useSWR("/api/activities");
   const router = useRouter();
-  const test = router.query;
   const { id } = router.query;
 
-  console.log("The current ID is " + id);
-  if (error) return <div>Failed to load</div>;
+  const { data, error } = useSWR(id ? `/api/activities/${id}` : null);
+
+  if (error) return <div>Something went wrong</div>;
   if (!data) return <div>Loading...</div>;
 
-  const currentActivity = data.data.find((activity) => activity.id === id);
   return (
-    <Fragment>
-      <ActivityDetail data={currentActivity}></ActivityDetail>
-    </Fragment>
+    <>
+      <ActivityDetail data={data.data} />
+    </>
   );
 }
