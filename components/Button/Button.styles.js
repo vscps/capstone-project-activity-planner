8b6270e7c764cfc60d8052b73705bc9a14aaf557
Daniel <daniel.seebacher17@gmail.com>
2025-06-10 13:04:41 +0200

    Refactored the button component to support multiple purposes and added Edit and Delete buttons to the activity details page

--- components/Button/Button.styles.js
+++ components/Button/Button.styles.js
@@ -1,6 +1,12 @@
 import styled from "styled-components";
+import keyframes from "styled-components";
 import { MdBookmarkAdd } from "react-icons/md";
 import { MdBookmarkAdded } from "react-icons/md";
+import { MdDelete } from "react-icons/md";
+import { MdEdit } from "react-icons/md";
+import { MdCancel } from "react-icons/md";
+import { GiConfirmed } from "react-icons/gi";
+import { MdArrowForward } from "react-icons/md";
 
 export const FavoriteIcon = styled(MdBookmarkAdd)`
   margin: 5px;
@@ -20,10 +26,63 @@ export const RemoveFavoriteIcon = styled(MdBookmarkAdded)`
   filter: drop-shadow(2px 2px 2px #191919);
 `;
 
+export const DeleteIcon = styled(MdDelete)`
+  margin: 5px;
+  color: white;
+  font-size: 25px;
+  cursor: pointer;
+  filter: drop-shadow(2px 2px 2px #191919);
+`;
+
+export const EditIcon = styled(MdEdit)`
+  margin: 5px;
+  color: white;
+  font-size: 25px;
+  cursor: pointer;
+  filter: drop-shadow(2px 2px 2px #191919);
+`;
+
+export const SubmitIcon = styled(MdArrowForward)`
+  margin: 5px;
+  color: white;
+  font-size: 25px;
+  cursor: pointer;
+  filter: drop-shadow(2px 2px 2px #191919);
+`;
+
+export const CancelIcon = styled(MdCancel)`
+  margin: 5px;
+  color: white;
+  font-size: 25px;
+  cursor: pointer;
+  filter: drop-shadow(2px 2px 2px #191919);
+`;
+
+export const ConfirmIcon = styled(GiConfirmed)`
+  margin: 5px;
+  color: white;
+  font-size: 25px;
+  cursor: pointer;
+  filter: drop-shadow(2px 2px 2px #191919);
+`;
+
 export const StyledButton = styled.button`
   align-items: center;
   background-clip: padding-box;
-  background-color: #83b36c;
+  background-color: ${(props) =>
+    props.$purpose === "favorite"
+      ? "#83b36c"
+      : props.$purpose === "delete"
+      ? "#f44336"
+      : props.$purpose === "edit"
+      ? "#FFC200"
+      : props.$purpose === "submit"
+      ? "#FFC200"
+      : props.$purpose === "cancel"
+      ? "#f44336"
+      : props.$purpose === "confirm"
+      ? "#83b36c"
+      : ""};
   border: 1px solid transparent;
   border-radius: 0.25rem;
   box-shadow: rgba(0, 0, 0, 0.02) 0 1px 3px 0;
@@ -37,7 +96,7 @@ export const StyledButton = styled.button`
   font-weight: 600;
   justify-content: center;
   line-height: 1.25;
-  margin: 0;
+  margin: 15px 15px 15px 0;
   min-height: 3rem;
   padding: calc(0.875rem - 1px) calc(1.5rem - 1px);
   position: relative;
@@ -49,19 +108,75 @@ export const StyledButton = styled.button`
   vertical-align: baseline;
   width: auto;
 
-  &:hover,
-  &:focus {
-    background-color: #83b36c;
+  &:not(:disabled):hover,
+  &:not(:disabled):focus {
+    background-color: ${(props) =>
+      props.$purpose === "favorite"
+        ? "#83b36c"
+        : props.$purpose === "delete"
+        ? "#f44336"
+        : props.$purpose === "edit"
+        ? "#FFC200"
+        : props.$purpose === "submit"
+        ? "#FFC200"
+        : props.$purpose === "cancel"
+        ? "#f44336"
+        : props.$purpose === "confirm"
+        ? "#83b36c"
+        : ""};
     box-shadow: rgba(0, 0, 0, 0.1) 0 4px 12px;
   }
 
-  &:hover {
+  &:not(:disabled):hover {
     transform: translateY(-1px);
   }
 
-  &:active {
-    background-color: #83b36c;
+  &:not(:disabled):active {
+    background-color: ${(props) =>
+      props.$purpose === "favorite"
+        ? "#83b36c"
+        : props.$purpose === "delete"
+        ? "#f44336"
+        : props.$purpose === "edit"
+        ? "#FFC200"
+        : props.$purpose === "submit"
+        ? "#FFC200"
+        : props.$purpose === "cancel"
+        ? "#f44336"
+        : props.$purpose === "confirm"
+        ? "#83b36c"
+        : ""};
     box-shadow: rgba(0, 0, 0, 0.06) 0 2px 4px;
     transform: translateY(0);
+  }
+
+  &:disabled {
+    background-color: #ddd;
+    color: #999;
+    cursor: not-allowed;
+    box-shadow: none;
+    transform: none;
+    opacity: 0.6;
+  }
+`;
+
+export const LoadingSpinner = styled.div`
+  width: 25px;
+  height: 25px;
+  border: 4px solid #fff;
+  border-bottom-color: #ff3d00;
+  border-radius: 50%;
+  display: inline-block;
+  box-sizing: border-box;
+  animation: rotation 1s linear infinite;
+  margin-right: 5px;
+
+  @keyframes rotation {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
   }
 `;
